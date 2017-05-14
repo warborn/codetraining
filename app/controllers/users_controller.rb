a251923cba3a4b5c86f38e606e77bb7f08f0f161
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2017-05-14 01:23:22 +0000

    Show user solutions ordered by challenge and newest first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 	before_action :authenticate_user!
 
 	def solutions
-		@solutions = current_user.solutions
+		@solutions = current_user
+									.solutions
+									.includes(translation: [:challenge, :language])
+									.order('challenges.id, solutions.created_at DESC')
 	end
 end
